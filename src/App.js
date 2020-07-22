3774f46eb15e8af774a4fce3324425c8039e4af8
ryguy806 <L1ghtSab3r11!!>
2020-07-22 14:23:04 -0700

    Added board, snake, and apple.

--- src/App.js
+++ src/App.js
@@ -10,6 +10,12 @@ import {
 } from './constants';
 
 const App = () => {
+  const canvasRef = useRef();
+  const [snake, setSnake] = useState(SNAKE_START);
+  const [apple, setApple] = useState(APPLE_START);
+  const [dir, setDir] = useState([0,-1]);
+  const [speed, setSpeed] = useState();
+  const [gameOver, setGameOver] = useState(false);
   
   const startGame = () => {
 
@@ -40,11 +46,26 @@ const App = () => {
   };
 
   useEffect(() => {
-
+    const context = canvasRef.current.getContext("2d");
+    context.setTransform(SCALE, 0, 0, SCALE, 0, 0);
+    context.clearRect(0,0,CANVAS_SIZE[0], CANVAS_SIZE[1]);
+    context.fillStyle = "blue";
+    snake.forEach(([x,y]) => context.fillRect(x, y, 1, 1));
+    context.fillStyle = "red";
+    context.fillRect(apple[0], apple[1], 1, 1);
   }, [snake, apple, gameOver]);
 
   return (
-    <div>Snake Game</div>
+    <div role="button" tab="0" onKeyDown={e => moveSnake(e)}>
+      <canvas
+        style={{ border: "1px solid black"}}
+        ref={canvasRef}
+        width={`${CANVAS_SIZE[0]}px`}
+        height={`${CANVAS_SIZE[1]}px`}
+      />
+      {gameOver && <div>GAME OVER!</div>}
+      <button onClick={startGame} >Start Game</button>
+    </div>
   )
 }
 
